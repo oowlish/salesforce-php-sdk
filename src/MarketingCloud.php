b5f2723f23f1b59bc7467873c49e123ddd838880
Luítame de Oliveira <luitame.oliveira@petcoach.co>
2018-11-08 10:28:43 -0300

    implementing the base class client where all the work will be held

--- src/MarketingCloud.php
+++ src/MarketingCloud.php
@@ -1,7 +1,58 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace Oowlish\Salesforce;
+declare(strict_types=1);
+
+namespace PetCoach\Salesforce;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use PetCoach\Salesforce\Endpoints\AbstractEndpoint;
 
 class MarketingCloud
 {
+    const BASE_URI = 'https://www.exacttargetapis.com';
+    const AUTH_URI = 'https://auth.exacttargetapis.com/v1';
+
+    /**
+     * @var string
+     */
+    private $clientId;
+
+    /**
+     * @var string
+     */
+    private $clientSecret;
+
+    /**
+     * @var Client|null
+     */
+    private $guzzle;
+
+    /**
+     * @param string $clientId
+     * @param string $clientSecret
+     * @param ClientInterface|null $guzzle
+     */
+    public function __construct(string $clientId, string $clientSecret, ClientInterface $guzzle = null)
+    {
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->guzzle = $guzzle ?: new Client(['base_uri' => self::BASE_URI]);
+    }
+
+    /**
+     * @param AbstractEndpoint $endpoint
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @return array
+     */
+    public function dispatch(AbstractEndpoint $endpoint): array
+    {
+        $response = $this->guzzle->request(
+            $endpoint->getHttpMethod(),
+            $endpoint->getURI(),
+            ['json' => $endpoint->getBody()]
+        );
+
+        return [$response->getBody()->getContents()];
+    }
 }
