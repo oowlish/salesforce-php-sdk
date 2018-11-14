c87634ec1a1bbbe7d48ef10520364e4f2a19afc6
Luítame de Oliveira <luitame@gmail.com>
2018-11-14 15:16:19 -0300

    seding a triggered email (#9)
    * seding a triggered email
    
    * fix the style
    
    * add space after the last param on the PHPDoc Blocks to fix the style
    
    * ideting the docs to fix style ;)
    
    * Update src/Api/MarketingCloud/Messaging.php

---
+++ src/Api/MarketingCloud/Messaging.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oowlish\Salesforce\Api\MarketingCloud;
+
+use Oowlish\Salesforce\Api\Resource;
+
+class Messaging extends Resource
+{
+    /**
+     * Sends transactional email using Marketing Cloud's triggered send functionality.
+     * In order to use this service, configure a triggered send definition in Email Studio.
+     *
+     * @param string $triggeredSendDefinitionId
+     * @param array  $from
+     * @param array  $to
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function sendEmail(string $triggeredSendDefinitionId, array $from, array $to, array $options = []): array
+    {
+        return $this
+            ->marketingCloud
+            ->request(
+                'POST',
+                "/messaging/v1/messageDefinitionSends/{$triggeredSendDefinitionId}/send",
+                [
+                    $from,
+                    $to,
+                    ['Options' => $options],
+                ]
+            );
+    }
+}
