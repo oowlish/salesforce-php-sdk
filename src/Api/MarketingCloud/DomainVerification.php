e1d3c007d62fd803e249b108a655ad280d40963d
Luítame de Oliveira <luitame.oliveira@petcoach.co>
2018-11-14 11:50:59 -0300

    adding new resource to Domain Verification Bulk Insert

---
+++ src/Api/MarketingCloud/DomainVerification.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oowlish\Salesforce\Api\MarketingCloud;
+
+use Oowlish\Salesforce\Api\Resource;
+
+class DomainVerification extends Resource
+{
+    /**
+     * Queue a bulk insert to the From Address Management table using either
+     * an array of email addresses or a data extension and column reference.
+     *
+     * @param string      $notificationEmail
+     * @param array       $addresses
+     * @param string|null $dETable
+     * @param string|null $dEColumn
+     *
+     * @return mixed
+     */
+    public function domainVerificationBulkInsert(
+        string $notificationEmail,
+        array $addresses = [],
+        string $dETable = null,
+        string $dEColumn = null
+    ) {
+        return $this->request(
+            'POST',
+            '/messaging/v1/domainverification/bulk/insert',
+            [
+                'NotificationEmail' => $notificationEmail,
+                'Addresses' => $addresses,
+                'DETable' => $dETable,
+                'DEColumn' => $dEColumn,
+            ]
+        );
+    }
+}
